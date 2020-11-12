41c8d34d0f4e4ea1876f3b912e24f99a2db07fb8
Karin Onwong <extendskarin18@gmail.com>
2020-11-13 02:25:37 +0700

    can upload but can't read 😂

--- server/app.js
+++ server/app.js
@@ -3,20 +3,18 @@ const multer = require("multer");
 const fs = require("fs");
 const bdps = require("body-parser");
 const app = express();
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
+const path = require("path");
 
 app.use(bdps.json());
-// app.use(
-//   multer({
-//     dest: __dirname + "/uploads/",
-//     limits: {
-//       fileSize: 100000,
-//     },
-//     onFileSizeLimit: function (file) {
-//       console.log("Failed: " + file.originalname + " is limited");
-//       fs.unlink(file.path);
-//     },
-//   })
-// );
 
 app.get("/", function (req, res) {
   // res.sendFile(__dirname + '/index.html')
@@ -24,9 +22,19 @@ app.get("/", function (req, res) {
   res.send("<p>hello</p>");
 });
 
-app.post('/upload', function (req, res) {
-  res.send(req.files)
-})
+// app.post('/upload', function (req, res) {
+//   res.send(req.files)
+// })
+
+app.post("/upload", upload.single("file"), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log("upload");
+  console.log(req.file, req.body);
+  let data = fs.createReadStream(path.join(__dirname, req.file.path), "utf8");
+  // const jsonObject = JSON.parse(data);
+  res.json(data);
+});
 
 app.listen(9876, function () {
   console.log("certificate-validate-server running on port 9876");
