21e67d20efc23789e802542d8093ca74656b770b
Karin Onwong <extendskarin18@gmail.com>
2020-11-21 00:55:54 +0700

    can receive link and get web ssl certificate

--- server/app.js
+++ server/app.js
@@ -3,6 +3,22 @@ const multer = require("multer");
 const fs = require("fs");
 const bdps = require("body-parser");
 const app = express();
+
+var getSslCertificate = require("get-ssl-certificate");
+getCert = async () =>  {
+  await getSslCertificate.get(req.body.link).then(function (certificate) {
+    console.log(certificate);
+
+    console.log(certificate.issuer);
+
+    console.log(certificate.valid_from);
+
+    console.log(certificate.valid_to);
+
+    console.log(certificate.pemEncoded);
+  });
+}
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
@@ -19,17 +35,30 @@ app.use(bdps.json());
 app.get("/", function (req, res) {
   // res.sendFile(__dirname + '/index.html')
   console.log("default path");
-  res.send("<p>hello</p>");
+  // res.send("<p>hello</p>");
 });
 
 app.post("/upload", upload.single("file"), function (req, res, next) {
   console.log("upload");
-  console.log(req.file, req.body);
+  // console.log(req.file, req.body);
   res.contentType("application/pdf");
-  let data = fs.createReadStream(path.join(__dirname, req.file.path), "utf8");
+  // let data = fs.createReadStream(path.join(__dirname, req.file.path), "utf8");
   // const jsonObject = JSON.parse(data);
   // res.json(data);
-  data.pipe(res);
+  // data.pipe(res);
+  let fileContent = fs.readFileSync(path.join(__dirname, req.file.path));
+  console.log(fileContent)
+  res.send(fileContent);
+});
+
+app.post("/registweb", function (req, res) {
+  try {
+    console.log("Hello ");
+    console.log(req.body.link);
+    getCert();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(9876, function () {
