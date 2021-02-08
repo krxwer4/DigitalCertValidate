f163e9b75e5ea624a098e0663c444774ba5416f2
Karin Onwong <extendskarin18@gmail.com>
2021-02-08 22:39:25 +0700

    can send transaction from back-end
    but not have metamask's confirmation

--- server/app.js
+++ server/app.js
@@ -4,36 +4,46 @@ const fs = require("fs");
 const jsSHA = require("jssha");
 const bdps = require("body-parser");
 const app = express();
-const Web3 = require('web3');
-const contArtifact = require('../client/src/artifacts/Poe.json');
-const contJson = contArtifact;
-const cors = require("cors")
+const Web3 = require("web3");
+const myContract = require("../client/src/artifacts/Poe.json");
+// const contJson = myContract;
+const cors = require("cors");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 // console.log(contArtifact)
 app.use(bdps.json());
 
-let provider = new Web3.providers.HttpProvider("http://localhost:2804");
-web3 = new Web3(provider);
-const contAddr = "0x3bfF0bfCE2a6630542DF02305832dcDaE8ed6C2b";
-const contract = new web3.eth.Contract([contJson], contAddr);
+const provider = new Web3.providers.HttpProvider("http://localhost:2805");
+const web3 = new Web3(provider);
+var id = {}
+var contract = {}
 
+const init = async () => {
+  id = await web3.eth.net.getId();
+  // const contAddr = "0x3bfF0bfCE2a6630542DF02305832dcDaE8ed6C2b";
+  const deployNetwork = myContract.networks[id];
+  // console.log(deployNetwork)
+  contract = new web3.eth.Contract(myContract.abi, deployNetwork.address);
+  // console.log(contract.methods)
+};
 
-var getSslCertificate = require("get-ssl-certificate");
-getCert = async () =>  {
-  await getSslCertificate.get(req.body.link).then(function (certificate) {
-    console.log(certificate);
+init();
 
-    console.log(certificate.issuer);
+// var getSslCertificate = require("get-ssl-certificate");
+// getCert = async () => {
+//   await getSslCertificate.get(req.body.link).then(function (certificate) {
+//     console.log(certificate);
 
-    console.log(certificate.valid_from);
+//     console.log(certificate.issuer);
 
-    console.log(certificate.valid_to);
+//     console.log(certificate.valid_from);
 
-    console.log(certificate.pemEncoded);
-  });
-}
+//     console.log(certificate.valid_to);
+
+//     console.log(certificate.pemEncoded);
+//   });
+// };
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -46,8 +56,6 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 const path = require("path");
 
-
-
 app.get("/", function (req, res) {
   // res.sendFile(__dirname + '/index.html')
   console.log("default path");
@@ -58,18 +66,23 @@ app.post("/gethash", upload.single("file"), async function (req, res, next) {
   // console.log("regist certificate");
   // console.log(req.file.path)
   // console.log(req.file.filename)
-  if(req.file.filename.length > 0){
+  // const account = await web3.eth.getAccounts();
+  console.log(req.body.account)
+  if (req.file.filename.length > 0 && req.body.account !== 'undefine') {
     let readFile = await fs.readFileSync(path.join(__dirname, req.file.path));
     // console.log(req.file.path)
-    let  shaObj = new jsSHA("SHA-512", "ARRAYBUFFER");
+    let shaObj = new jsSHA("SHA-512", "ARRAYBUFFER");
     await shaObj.update(readFile);
-    let hashFile = "0x" + await shaObj.getHash("HEX");
-    console.log(hashFile)
+    let hashFile = "0x" + (await shaObj.getHash("HEX"));
+    // console.log(hashFile);
     // res.contentType("text");
-    res.send(hashFile);
-  }
-  else {
-    res.status(400).json("regist err")
+    const receipt = await contract.methods.addCertificate(hashFile).send({
+      from: req.body.account,
+    });
+    // res.send(hashfile);
+    res.send(receipt)
+  } else {
+    res.status(400).json("regist err");
   }
 });
 
